a97a47af238022f63a38a7e8a5d94717eec76e70
NataliaSilantieva <63507488+NataliaSilantieva@users.noreply.github.com>
2020-10-18 20:43:55 +0300

    Update task_6.js

--- js/task_6.js
+++ js/task_6.js
@@ -9,13 +9,13 @@ inputRef.addEventListener('blur',inputValidation);
 function inputValidation(){
     const dataLength = Number(inputRef.dataset.length);
     const inputLength = Number(inputRef.value.length); 
-    if ( inputLength < dataLength){
-        inputRef.classList.add('invalid')
-        inputRef.classList.remove('valid') 
+    if ( inputLength === dataLength){
+        inputRef.classList.add('valid')
+        inputRef.classList.remove('invalid') 
 
     } else{
-        inputRef.classList.add('valid') 
-        inputRef.classList.remove('invalid') 
+        inputRef.classList.add('invalid') 
+        inputRef.classList.remove('valid') 
     }
     
 }
